051f454274453cb7b5e04386392ac69c2a3c8ae7
Ivan Deschenaux <ivan.deschenaux@gmail.com>
2023-12-06 10:49:36 +0000

    Add gid_size for build_all_revelations

--- R/build_all_revelations.R
+++ R/build_all_revelations.R
@@ -6,7 +6,7 @@
 #' @param keep_anonymous Set to TRUE if you want to generate files for the games that were played in the "anonymous" condition. Defaults to FALSE.
 #' @export
 
-build_all_revelations <- function(path, remove_optouts = TRUE) {
+build_all_revelations <- function(path, remove_optouts = TRUE, gid_size = 4) {
   res_file <- paste0(path, "/Results/SubsetContributions-SummaryTable.csv")
 
   if (!file.exists(res_file)) {
@@ -32,6 +32,6 @@ build_all_revelations <- function(path, remove_optouts = TRUE) {
     giver <- res[r, "ID"]
     amount_offered <- res[r, "amtGiven"]
     anonymous <- ifelse(res[r, "Condition"] == "anonymous", TRUE, FALSE)
-    build_subset_expectation(path = path, subdir = "SubsetRevelations", pid = receiver, aid = giver, offer = amount_offered, anonymous = anonymous)
+    build_subset_expectation(path = path, subdir = "SubsetRevelations", pid = receiver, aid = giver, offer = amount_offered, anonymous = anonymous, gid_size = gid_size)
   }
 }
